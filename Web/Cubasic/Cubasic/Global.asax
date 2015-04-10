3d0ef009016d9a37d2e3b23f196310a268e04de0
Gang Yi <18666993010@163.com>
2015-04-10 10:37:07 +0800

    Initialize

---
+++ Web/Cubasic/Cubasic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cubasic.MvcApplication" Language="C#" %>
---
+++ Web/Cubasic/Cubasic/Scripts/customer/template.js
@@ -0,0 +1,102 @@
+(function ($) {
+    $(document).ready(function () {
+        $(".zoom").hoverZoom({
+            overlay: true, // false to turn off (default true)
+            overlayColor: '#FF6600', // overlay background color
+            overlayOpacity: 0.7, // overlay opacity
+            zoom: 0, // amount to zoom (px)
+            speed: 300 // speed of the hover
+         }); // Default
+
+        $.scrollUp({
+            scrollName: 'scrollUp',      // Element ID
+            scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+            scrollFrom: 'top',           // 'top' or 'bottom'
+            scrollSpeed: 300,            // Speed back to top (ms)
+            easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+            animation: 'fade',           // Fade, slide, none
+            animationSpeed: 200,         // Animation speed (ms)
+            scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
+            scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
+            scrollText: 'Scroll to top', // Text for element, can contain HTML
+            scrollTitle: false,          // Set a custom <a> title if required.
+            scrollImg: true,            // Set true to use image
+            activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+            zIndex: 2147483647           // Z-Index for the overlay
+        });
+
+        $(".banner-image").backstretch([
+            '../Images/customer/banner.jpg',
+            '../Images/customer/banner2.jpg',
+            '../Images/customer/banner3.jpg'
+            ], { fade: 750, duration: 4000 });
+
+        // Fixed header
+        //-----------------------------------------------
+        //trigger the scroll events 
+        $(window).scroll(function () {
+            if (($(".header.fixed").length > 0)) {
+                if (($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+                    $("body").addClass("fixed-header-on");
+                } else {
+                    $("body").removeClass("fixed-header-on");
+                }
+            };
+        });
+
+        $(window).load(function () {
+            if (($(".header.fixed").length > 0)) {
+                if (($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+                    $("body").addClass("fixed-header-on");
+                } else {
+                    $("body").removeClass("fixed-header-on");
+                }
+            };
+        });
+
+        //Scroll Spy
+        //Launch the scroll plug-in via JavaScript code after add "position: relative" in CSS
+        if ($(".scrollspy").length > 0) {
+            $("body").addClass("scroll-spy");
+            $('body').scrollspy({
+                target: '.scrollspy', // target navigation
+                offset: 152 // calculate the scroll position relative to the top offset(Pixel)
+            });
+        }
+
+        //Smooth Scroll
+        //-----------------------------------------------
+        if ($(".smooth-scroll").length > 0) {
+            $('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function () {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    if (target.length) {
+                        $('html,body').animate({
+                            scrollTop: target.offset().top - 151
+                        }, 1000);
+                        return false;
+                    }
+                }
+            });
+        }
+
+        // Animations
+        //-----------------------------------------------
+        if (($("[data-animation-effect]").length > 0) && !Modernizr.touch) {
+            $("[data-animation-effect]").each(function () {
+                var $this = $(this),
+				animationEffect = $this.attr("data-animation-effect");
+                if (Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
+                    $this.appear(function () {
+                        setTimeout(function () {
+                            $this.addClass('animated object-visible ' + animationEffect);
+                        }, 400);
+                    }, { accX: 0, accY: -130 });
+                } else {
+                    $this.addClass('object-visible');
+                }
+            });
+        };
+    }); // End document ready
+})(this.jQuery);
